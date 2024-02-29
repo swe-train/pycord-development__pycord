af29f1f19debed5b611be5111427996c25ebc84f
Lala Sabathil <lala@pycord.dev>
2024-02-29 01:10:06 +0100

    fix: either i'm high and it won't work, or i'm god and this works

--- discord/guild.py
+++ discord/guild.py
@@ -3167,13 +3167,15 @@ class Guild(Hashable):
         limit: int | None = 100,
         before: SnowflakeTime | None = None,
         after: SnowflakeTime | None = None,
-        oldest_first: bool | None = None,
         user: Snowflake = None,
         action: AuditLogAction = None,
     ) -> AuditLogIterator:
         """Returns an :class:`AsyncIterator` that enables receiving the guild's audit logs.
 
         You must have the :attr:`~Permissions.view_audit_log` permission to use this.
+
+        See `gateway <https://discord.com/developers/docs/resources/audit-log#get-guild-audit-log>`_
+        for more information about the `before` and `after` parameters.
 
         Parameters
         ----------
@@ -3187,9 +3189,6 @@ class Guild(Hashable):
             Retrieve entries after this date or entry.
             If a datetime is provided, it is recommended to use a UTC aware datetime.
             If the datetime is naive, it is assumed to be local time.
-        oldest_first: :class:`bool`
-            If set to ``True``, return entries in oldest->newest order. Defaults to ``True`` if
-            ``after`` is specified, otherwise ``False``.
         user: :class:`abc.Snowflake`
             The moderator to filter entries from.
         action: :class:`AuditLogAction`
@@ -3234,7 +3233,6 @@ class Guild(Hashable):
             before=before,
             after=after,
             limit=limit,
-            oldest_first=oldest_first,
             user_id=user_id,
             action_type=action,
         )
